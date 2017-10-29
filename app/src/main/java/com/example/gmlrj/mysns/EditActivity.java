1280ccc53dbccdb44dc1fb8907c0f9323494ea79
kimheegeon <32567051+kimheegeon@users.noreply.github.com>
2017-10-29 21:31:09 +0900

    글쓰기에서 뒤로가기

--- app/src/main/java/com/example/gmlrj/mysns/EditActivity.java
+++ app/src/main/java/com/example/gmlrj/mysns/EditActivity.java
@@ -1,5 +1,7 @@
 package com.example.gmlrj.mysns;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +15,8 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -25,11 +29,11 @@ public class EditActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
-            case R.id.action_back:
-                Toast.makeText(this,"back",Toast.LENGTH_SHORT).show();
-                return true;
             case R.id.action_submit:
                 Toast.makeText(this,"submit",Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
