5c6bd94eac907bb2fa68f0b03dfbc0cacc2bdb50
류소현 <thgus9466@naver.com>
2017-11-14 19:14:17 +0900

    비밀번호 바꾸기

---
+++ app/src/main/java/com/example/gmlrj/mysns/ChangePWActivity.java
@@ -0,0 +1,13 @@
+package com.example.gmlrj.mysns;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChangePWActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pw);
+    }
+}
--- app/src/main/java/com/example/gmlrj/mysns/SettingFragment.java
+++ app/src/main/java/com/example/gmlrj/mysns/SettingFragment.java
@@ -1,14 +1,17 @@
 package com.example.gmlrj.mysns;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.app.Fragment;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.setting_fragment, container, false);
@@ -28,6 +31,17 @@ public class SettingFragment extends Fragment {
         adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_edit), "Setting") ;
         adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_edit), "Setting") ;
         adapter.addItem(ContextCompat.getDrawable(getActivity(), R.drawable.ic_edit), "Setting") ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                if(position==0){
+                    Intent intent = new Intent(getActivity(), ChangePWActivity.class);
+                    SettingFragment.this.startActivity(intent);
+                    Log.d("Intent Call","Call List" + position);
+                }
+                Log.d("Item Click Event", "Click" + position);
+            }
+        });
         return view;
     }
 }
